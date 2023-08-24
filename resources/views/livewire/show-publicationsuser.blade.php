c41dcdf5e9bc9d2a956c3216cee2964273175de6
NEBUR342 <ralvarezfernandez342@gmail.com>
2023-08-24 10:52:06 +0200

    barra de busqueda corregida (diseño)

--- resources/views/livewire/show-publicationsuser.blade.php
+++ resources/views/livewire/show-publicationsuser.blade.php
@@ -1,5 +1,5 @@
 <div class='min-[480px]:px-12 mt-4 cursor-default'>
-    <div class="flex min-[480px]:mx-12 mb-3">
+    <div class="flex mb-3">
         <div class="flex-1">
             <x-input class="w-full" type='search' placeholder="Buscar publicaciones..." wire:model="buscar"></x-input>
         </div>

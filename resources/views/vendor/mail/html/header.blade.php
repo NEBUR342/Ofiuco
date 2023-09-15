e8f1e20f8be40948f5327a3b99d76aede4a17262
NEBUR342 <ralvarezfernandez342@gmail.com>
2023-09-15 11:02:26 +0200

    actualizo contacto

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,12 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{Storage::url('ofiucologo.jpg')}}" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+                <img src="{{ Storage::url('fantasmaclaro.jpg') }}" class="logo" alt="Laravel Logo">
+            @else
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>

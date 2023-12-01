b4f507d08c13b43032baf1ec02be517dbe39bec6
NEBUR342 <ralvarezfernandez342@gmail.com>
2023-12-01 11:27:56 +0000

    command en crontab

---
+++ app/Console/Commands/DeleteFoldertmp.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+class DeleteFoldertmp extends Command{
+    protected $signature = 'command:delete-foldertmp';
+    protected $description = 'Eliminar la carpeta temporal que guarda imágenes';
+    public function handle()
+    {
+        File::deleteDirectory(storage_path('app/public/livewire-tmp'));
+    }
+}
